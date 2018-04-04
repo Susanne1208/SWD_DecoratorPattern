36dd3e5445b14680a034934620c7301376cb187d
Susanne Olsen <susanne_olsen@live.dk>
2018-04-04 12:12:45 +0200

    Added few classes and stuff

---
+++ PizzaDecorator/Handin3/Margherita.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handin3
+{
+    public class Margherita : Pizza
+    {
+        public string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double getCost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PizzaDecorator/Handin3/ToppingDecorator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handin3
+{
+    public abstract class ToppingDecorator : Pizza
+    {
+        public abstract string getDescription();
+    }
+}
