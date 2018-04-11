db3ece2b039b01d24059205fe544e8147e2cf2e1
armina <armina1506@hotmail.com>
2018-04-11 11:44:47 +0200

    applikation needed

--- PizzaDecorator/Handin3/Program.cs
+++ PizzaDecorator/Handin3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,12 +14,48 @@ namespace Handin3
             //Pizza myPizza = new Margherita();
             //Console.WriteLine(myPizza.getDescription() + myPizza.Cost());
 
+            string response;
+            string responseP;
             Pizza myPizza1 = new Margherita();
-            myPizza1 = new Pineapple(myPizza1);
-            myPizza1 = new Pepperoni(myPizza1);
-            Console.WriteLine(myPizza1.getDescription() + myPizza1.Cost());
-           
+
+            Console.WriteLine("Would you like to add something to your pizza? (y/n)");
+            response = Console.ReadLine();
+            if (response == "y")
+            {
+                do ()
+                Console.WriteLine("Which topping");
+
+            }
+            else
+            {
+
+            }
+            switch (response)
+            {
+                case "y":
+                    Console.WriteLine("Pepperoni or Pineapple?");
+                    responseP = Console.ReadLine();
+                    if (responseP == "pepperoni")   
+                    {
+                        myPizza1 = new Pepperoni(myPizza1);
+                        Console.WriteLine(myPizza1.getDescription() + myPizza1.Cost());
+                    }
+
+                    if (responseP == "pineapple")
+                    {
+                        myPizza1 = new Pineapple(myPizza1);
+                        Console.WriteLine(myPizza1.getDescription()+myPizza1.Cost());
+                    }
+                    break;
+                case "n":
+                    break;
+                default:
+                    Console.WriteLine("Write 'y' or 'n' to continue");
+                    break;
+            }
 
         }
+
+        
     }
 }
